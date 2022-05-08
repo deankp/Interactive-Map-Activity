69da959bf3aa64262fec152f77c5134a53d5d43b
deankp <dperez0689@gmail.com>
2022-05-08 12:28:06 -0500

    added coordinate function

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // map object
 
 // get coordinates via geolocation api
+// Get the user's coordinates:                                                              
+async function getCoords(){
+    pos = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+    })
+    return [pos.coords.latitude, pos.coords.longitude]
+}                              
+
 
 // get foursquare businesses
 
